f56e55a3c60cb0d86b343290e85802e3d5572262
João Victor <joaovictordesena2020@gmail.com>
2024-12-29 00:27:52 -0300

    melhorias no formulário com validação e responsividade

--- src/js/script.js
+++ src/js/script.js
@@ -1,47 +1,19 @@
 const campos = document.querySelectorAll(".campo-formulario");
-const botaoEnvio = document.querySelector(".botao-de-envio");
+const botaoEnviar = document.querySelector(".botao-de-envio");
 const mensagensErro = document.querySelectorAll(".campo-obrigatorio");
 
-botaoEnvio.addEventListener("click", (evento) => {
+botaoEnviar.addEventListener("click", (evento) => {
     evento.preventDefault();
-    campos.forEach((campo, indice) => {
-        const mensagemErro = mensagensErro[indice];
-
-        if(campo.value === "") {
-            campo.classList.remove("valido");
+    campos.forEach((campo) => { 
+        const mensagemErro = campo.nextElementSibling; 
+        if (campo.value.trim() === "") {
             campo.classList.add("invalido");
+            campo.classList.remove("valido");
             mensagemErro.classList.add("ativo");
-
-            
         } else {
             campo.classList.remove("invalido");
             campo.classList.add("valido");
             mensagemErro.classList.remove("ativo");
-
-            
-           
         }
     });
-});
-
-
-function tratarMudanca(evento) {
-    const campo = evento.target;
-    const indice = Array.from(campos).indexOf(campo);
-    const mensagemErro = mensagensErro[indice];
-
-
-    if(campo.value === "") {
-        campo.classList.add("invalido");
-        mensagemErro.classList.add("ativo");
-
-    } else {
-        campo.classList.remove("invalido");
-        mensagemErro.classList.remove("ativo");
-
-    }
-}
-
-campos.forEach((campo) => {
-    campo.addEventListener("input", tratarMudanca);
 });
